32c47bf980deaadda312948dbc96a7637c461a70
chua kai xiang <cchuakaixiang@gmail.com>
2025-04-02 14:01:14 +0800

    new

---
+++ userprofile_navbar.php
@@ -0,0 +1,66 @@
+<div class="userprofile-navigation-bar-div">
+
+
+
+    <a href="user profile.html">My Profile</a>
+    <a href="userprofile_address.html">Addresses</a>
+   <a href="my_orders.php">My Orders</a>
+    <a href="view_history.php">View History</a>
+    <a href="reset_password.php">Reset Password</a>
+
+     <a class="" href="">
+        <img class="" src=""> My coupons
+    </a>
+    <a class="" href="">
+        <img class="" src=""> Need Help?
+    </a>
+
+    <a class="log-out-a" href="">
+        <img class="log_out_image" src="log_out.png"> Log out
+    </a>
+
+
+   
+
+</div>
+
+<style>
+    .userprofile-navigation-bar-div{
+    background-color: #333;
+    display: flex;
+    flex-direction: column;
+    padding: 5px;
+ 
+}
+
+a{
+    color: white;
+    padding: 10px 20px;
+    text-decoration: none;
+    margin: 5px;
+}
+a:hover{
+    background-color: #555;
+}
+a:active{
+    background-color: white;
+    color: #333;
+}
+.log-out-a{
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    margin: 5px;
+    text-align: center;
+    width: 200px;
+
+}
+.log_out_image{
+    width: 20px;
+    height: auto;
+   margin-right: 5px;
+}
+</style>
